d01c4ff3f711d1b17280221f686fd0db27b9991f
barakatadediran <adediranbarakat@gmail.com>
2022-10-21 11:31:59 -0400

    Starting IGN Data Analysis

---
+++ IGNGaming_Analysis.R
@@ -0,0 +1,11 @@
+# ----------------------------------------- #
+#             Project Neos                  #
+#     SubTitle: IGN Gaming Analysis         #
+#   Looking at Games and Their Ranking      #
+# ----------------------------------------- #
+
+
+library(tidyverse)
+library(ggplot2)
+
+df = read.csv('~/R/ProjectNeos/Data/IGN_data.csv')
